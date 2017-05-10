ec294bed3454d145de62dc03afa5c3e5972c5868
mrmyakish <xviterx@gmail.com>
2017-05-10 15:06:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
-  $(".Phone").hover(function(){
-    $(".Phone_img").css("visibility","visible")
-    $( ".Phone_img" ).animate({
-      opacity: 1   })
+$(".Phone").hover(function(){
+  $( ".Phone_img" ).animate({opacity: 1})
+  $( ".Order_img" ).animate({opacity: 0})
+  $( ".Pokeman_img" ).animate({opacity: 0})
 });
 $(".Phone").mouseleave(function(){
-  $(".Phone_img").css("visibility","hidden")
-  $( ".Phone_img" ).animate({
-    opacity: 0   })
+  $( ".Phone_img" ).animate({opacity: 0})
+  $( ".Pokeman_img" ).animate({opacity: 0})
+  $( ".Order_img" ).animate({opacity: 0})
+
 });
 $(".Pokeman").hover(function(){
-  $(".Pokeman_img").css("visibility","visible")
-  $( ".Pokeman_img" ).animate({
-    opacity: 1   })
+  $( ".Pokeman_img").animate({opacity: 1})
+  $( ".Phone_img" ).animate({opacity: 0})
+  $( ".Order_img" ).animate({opacity: 0})
+
 });
 $(".Pokeman").mouseleave(function(){
-  $(".Pokeman_img").css("visibility","hidden")
-  $( ".Pokeman_img" ).animate({
-    opacity: 0   })
+  $( ".Pokeman_img" ).animate({opacity: 0})
+  $( ".Phone_img" ).animate({opacity: 0})
+  $( ".Order_img" ).animate({opacity: 0})
   });
+$(".Order").hover(function(){
+  $( ".Order_img" ).animate({opacity: 1})
+  $( ".Phone_img" ).animate({opacity: 0})
+});
+$(".Order").mouseleave(function(){
+  $( ".Order_img" ).animate({opacity: 0})
+  $( ".Phone_img" ).animate({opacity: 0})
+    });
 })
